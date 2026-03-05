401a0374c2aba53d10bb22fab0a851aee70347f9
tumtae <lfpicolirosa@gmail.com>
2026-03-05 10:29:28 -0300

    abu

--- Assets/teste.cs
+++ Assets/teste.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Variável para controlar a velocidade do movimento
     public float speed = 5f;
+    private Rigidbody rb; //referencia ao componente Rigidbody do personagem
 
     void Update()
     {
+        rb = GetComponent<Rigidbody>(); //Obtém o componente Rigidbody do personagem
+        //1. Captura do input do jogador (Teclas WASD ou setas)
         float moveZ = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(0f, moveY, moveZ); // Para 2D
-        // Para 3D, use: new Vector3(moveX, 0f, moveY);
-
-        transform.Translate(movement * speed * Time.deltaTime);
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, moveZ * speed); //Aplica o movimento no eixo Z
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, moveY * speed, rb.linearVelocity.z); //Aplica o movimento no eixo Y
+    
     }
 }
